5ef66a7cb250e2f3c487ff161daf7c7071954ae1
msuhyeon <job.maengsh@gmail.com>
2022-09-06 00:53:35 +0900

    todo app init

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader"
+import TodoFooter from "./components/TodoFooter"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+export default {
+  name: 'app',
+  components: {
+    // 컴포넌트 태그명: 컴포넌트 내용
+    "TodoHeader": TodoHeader,
+    "TodoFooter": TodoFooter,
+    "TodoInput": TodoInput,
+    "TodoList": TodoList
+  }
+}
+</script>
+
+<style>
+
+</style>
